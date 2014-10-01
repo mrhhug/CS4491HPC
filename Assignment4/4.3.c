a872cfa4d5eb4f6cef9a53889adb4ebc2c11e0cb
mrhhug@gmail.com <mrhhug@gmail.com>
2014-10-01 00:47:41 -0400

    commit from script

--- Assignment4/4.3.c
+++ Assignment4/4.3.c
@@ -1,15 +1,17 @@
-#include <time.h>
-#include <math.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
-#include <limits.h>
+#include <math.h>
 
-long long int number_in_circle = 0;
-long long int number_of_tosses_per_thread = -1;
+double delta=1;
 int thread_count = -1; 
+double area=0;
 pthread_mutex_t mut;
 
+struct my_thread_rank
+{
+	int rank;
+};
 int get_max_threads()
 {
 	int max_string_size = 10; //including null char
@@ -34,51 +36,54 @@ int get_max_threads()
 			max = atoi(str);
 		}
 	}
-	ret = fclose(fp);
-	if (0!=ret)
+	int retur = fclose(fp);
+	if (0!=retur)
 	{
 		printf("file close error\n");
 	}
 	return max;
 }
-double grabRand() 
+double trap_area(double y1,double y2,double delta)
 {
-	double div = RAND_MAX / 2;
-	return -1 + (rand() / div);
+	return .5*(y1+y2)*delta;
 }
-void *monty(void* _)
+double eval_function(double x)
 {
-	double distance_squared,x,y;
-	long long int toss;
-	long long int mycircleCount =0;
-	for (toss = 0; toss < number_of_tosses_per_thread; toss++)
+	return sqrt(1-x*x);
+}
+void* my_thread(void * data)
+{
+	struct my_thread_rank *info = data;
+	int my_rank = info ->rank;
+	double lower_bound=my_rank * 1/(double)thread_count;
+	double upper_bound=(1+my_rank) * 1/(double)thread_count;
+	double y1;
+	double y2;
+	double my_area = 0.0;
+	double i;
+	y2 = eval_function(lower_bound);
+	for(i=lower_bound+delta;i<=upper_bound;i+=delta)
 	{
-		x = grabRand();
-		y = grabRand();
-		distance_squared = x*x + y*y;
-		if (distance_squared <= 1)
-		{
-			mycircleCount++;
-		}
+		y1=y2;
+		y2=eval_function(i);
+		my_area += trap_area(y1,y2,delta);
 	}
 	pthread_mutex_lock(&mut);
-	number_in_circle+=mycircleCount; 
+	area+=my_area; 
 	pthread_mutex_unlock(&mut);
-	return NULL;
+	free(info);
+	return 0;
+
 }
 int main(int argc, char *argv[])
 {
 	int i, ret;
 	int max_threads;
-	int thread_count;
-	double number_of_tosses;
-	double pi_estimate;
 	pthread_t* threads;
-	srand(time(NULL)); //void return;
 
 	if (3 != argc)
 	{
-		printf("I want 2 positional arguments : thread count & tosses per thread\n");
+		printf("I want 2 positional arguments : thread count & the delta of the trap(ex .00001)\n");
 		return 1;
 	}
 	max_threads = get_max_threads();
@@ -93,10 +98,10 @@ int main(int argc, char *argv[])
 		printf("Must supply an integer thread count between 1 and your system's max inclusivly\n");
 		return 3;
 	}
-	number_of_tosses_per_thread = atoll(argv[2]);
-	if(1 > number_of_tosses_per_thread || number_of_tosses_per_thread>pow(10,52))
+	delta = atof(argv[2]);
+	if(0 > delta || 1 < delta)
 	{
-		printf("Supply a toss_per_thread between 1 and a Sexdecillion inclusivly\n");
+		printf("Supply a delta between 1 and zero\n");
 		return 4;
 	}
 	if(0!=pthread_mutex_init(&mut, NULL))
@@ -112,11 +117,18 @@ int main(int argc, char *argv[])
 	}
 	for(i=0;i<thread_count;i++)
 	{
-		ret = pthread_create(&threads[i], NULL, monty, NULL);
+		struct my_thread_rank *info = malloc(sizeof(struct my_thread_rank));
+		if(NULL==info)
+		{
+			printf("strut malloc failure");
+			return 7;
+		}
+		info->rank = i;
+		ret = pthread_create(&threads[i], NULL, my_thread,info);
 		if(0!=ret)
 		{
 			printf("thread creation fail\n");
-			return 7;
+			return 8;
 		}
 	}
 	for(i=0;i<thread_count;i++)
@@ -125,21 +137,18 @@ int main(int argc, char *argv[])
 		if(0!=ret)
 		{
 			printf("thread join fail\n");
-			return 8;
+			return 9;
 		}
 	}
 	ret = pthread_mutex_destroy(&mut);	
 	if(0!=ret)
 	{
 		printf("mutex destroy fail\n");
-		return 9;
+		return 10;
 	}
 	free(threads); //void return
-	//printf("%llu\n",number_in_circle);
-	printf("Total tosses : %llu\n",thread_count*number_of_tosses_per_thread);
-	number_of_tosses = thread_count*number_of_tosses_per_thread;
-	pi_estimate = 4*number_in_circle/number_of_tosses;
-	printf("Your estimation of PI : \t%.15f \n",pi_estimate);
-	printf("math.h's macro for  PI: \t%.15f\n", M_PI);
+	printf("Your trapezoidal estimation of PI: \t%.15f\n",area*4);
+	printf("math.h's macro for PI: \t\t\t%.15f\n", M_PI);
+
 	return 0;
 }
