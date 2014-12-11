9918e7eefc3a53dde543f0818dee6cd4993de679
mrhhug@gmail.com <mrhhug@gmail.com>
2014-12-10 19:56:24 -0500

    commit from script

--- final/pthread.c
+++ final/pthread.c
@@ -5,7 +5,7 @@
 #include <math.h>
 #include <pthread.h>
 
-#define thread_count 5;
+#define thread_count 5
 
 long long int num;
 
@@ -17,15 +17,25 @@ int main (int argc, char* argv[])
 {
 	if (argc < 2)
 		err_sys ("missing command line argument");
-	long long int num = strtoll (argv[1], 0 , 10);
+	num = strtoll (argv[1], 0 , 10);
 	if (num < 0)
 		err_sys ("invalid command line argument");
 	pthread_t threads[thread_count];
+	long long int i;
 	for (i=0; i<thread_count; i++)
-		if(pthread_create(&threads[i], NULL,find_num_primes, (void *)rank++))
+		if(pthread_create(&threads[i], NULL,find_num_primes, (void *)i))
 			err_sys("error creating thread");
-	long long int num_primes = find_num_primes (num);
-	printf ("%lld primes <= %lld\n", num_primes, num);
+	void* ret_val;
+	long long int global_sum = 0;
+	for (i=0; i<thread_count; i++)
+	{
+		if(pthread_join(threads[i],&ret_val))
+			err_sys("error creating thread");
+		int local_sum=(long long int) ret_val;
+		global_sum+=local_sum;
+
+	}
+	printf ("%lli primes <= %lli\n", global_sum, num);
 	return 0;
 }
 
@@ -38,14 +48,18 @@ int is_prime (long long int n)
 	return i > max;
 }
 
-void* find_num_primes (void* rank)
+void* find_num_primes (void* datum)
 {
+	long long int my_rank = (long long int) datum;
 	long long int result = 0;
-	long long int i;
-	for (i = 2; i <= n; i++)
+	long long int i = my_rank*num/thread_count;
+	long long int n = (my_rank+1)*num/thread_count;
+	if(2>i)
+		i=1;
+	for (i+=1; i <= n; i++)
 		if (is_prime (i))
 			result++;
-	return result;
+	return (void *) result;
 }
 
 void err_sys (const char* message)
